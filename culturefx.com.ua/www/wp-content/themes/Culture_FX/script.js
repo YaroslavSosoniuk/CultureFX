e385d5503e65f7c3f3dde4bcb68339a0cf3ec946
YaroslavSosoniuk <yaryk.s.v@gmail.com>
2017-12-13 08:53:19 +0200

    Slection only one checkbox from group on registration

--- culturefx.com.ua/www/wp-content/themes/Culture_FX/script.js
+++ culturefx.com.ua/www/wp-content/themes/Culture_FX/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    
     $('input[name="first_name"]').on("input", function(){
         var name = $(this).val();
         if(name != ""){
@@ -10,6 +9,37 @@ $(document).ready(function(){
         if(result.length < name.length){
             $(this).val("");
             }
+        }
+    });
+    $(".age_radio").bind("change", function () {
+        var checked_age = this;
+        if($(checked_age).prop('checked')) {
+            $('.age_points .age_radio').each(function(){
+                if($(this).prop('checked') && this != checked_age){
+                    $(this).prop('checked', false);
+                }
+            });
+        }
+    });
+    $(".gender_radio").bind("change", function () {
+        var checked_gender = this;
+        if($(checked_gender).prop('checked')) {
+            $('.gender_radio').each(function(){
+                if($(this).prop('checked') && this != checked_gender){
+                    $(this).prop('checked', false);
+                }
+            });
+        }
+    });
+
+    $('.c-level-input input').bind("change", function(){
+        var c_level_input = this;
+        if($(c_level_input).prop('checked')){
+            $('.c-level-input input').each(function(){
+                if($(this).prop('checked') && this != c_level_input){
+                    $(this).prop('checked', false);
+                }
+            });
         }
     });
 });
